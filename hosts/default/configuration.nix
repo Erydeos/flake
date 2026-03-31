22daf0d04a3607b0b4b5f7a7970f7ebb7f01b07a
Erydeos <ellwes04@gmail.com>
2026-03-31 15:21:16 +0200

    update

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -155,4 +155,13 @@ security.pam.services.login.enableGnomeKeyring = true;
 
 security.sudo.wheelNeedsPassword = false;
 
+fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    ipafont 
+    kochi-substitute
+  ];
+
 }
