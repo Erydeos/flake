328b41aeccb46cb67b5d8268b07f0cf92e7d7b4c
Erydeos <ellwes04@gmail.com>
2026-04-04 11:25:44 +0200

    update

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -20,6 +20,7 @@
         "sleep 1 && xrandr --output DP-1 --primary"
         "[workspace special:music silent] ytmdesktop --password-store='gnome-libsecret' --no-sandbox &"
         "[workspace special:notes silent] obsidian"
+        "hyprctl dispatch workspace 1"
         "quickshell"
       ];
     };
