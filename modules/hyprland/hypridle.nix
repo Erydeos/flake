4dd6c4c1834d879bf40add26dc074e51a119fef0
Erydeos <ellwes04@gmail.com>
2026-04-14 18:39:56 +0200

    update

--- modules/hyprland/hypridle.nix
+++ modules/hyprland/hypridle.nix
@@ -12,13 +12,8 @@
 
       listener = [
         {
-            "timeout" = "300";                                 # 5min
+            "timeout" = "600";                                 # 5min
             "on-timeout" = "loginctl lock-session";            # lock screen when timeout has passed
-        }
-        
-        {
-            "timeout" = "1800";                          # 30min
-            "on-timeout" = "systemctl suspend ";             # suspend pc
         }
       
       ];
